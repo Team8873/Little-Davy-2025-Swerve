bc8cf292e7b076925b8b0456021a03e7936a3f27
8873DriverStation <8873DriverStation@gmail.com>
2025-01-25 11:02:18 -0500

    Rename

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,10 +4,10 @@ public class Constants {
     public static class TestMotorConstants{
         public static final int TestMotorId = 15;
     }
-    public static class TimeOfFlightConstants{
+    public static class ToFConstants{
         public static final int SensorId = 16;
     }
-    
+
         
     }
      
--- src/main/java/frc/robot/subsystems/TimeOfFlightSensor.java
+++ src/main/java/frc/robot/subsystems/TimeOfFlightSensor.java
@@ -1,5 +1,5 @@
 package frc.robot.subsystems;
-import frc.robot.Constants.ToFConstants;;
+import frc.robot.Constants.ToFConstants;
 import com.playingwithfusion.TimeOfFlight;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
