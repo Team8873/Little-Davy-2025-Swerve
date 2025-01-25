0a6bda47097a5440ad707f7de3f81147ef4bc50d
8873DriverStation <8873DriverStation@gmail.com>
2025-01-25 09:15:44 -0500

    Created a constants file

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,14 @@
+package frc.robot;
+
+public class Constants {
+    public static class TestMotorConstants{
+        public static final int TestMotorId = 15;
+    }
+    public static class TimeOfFlightConstants{
+        public static final int SensorId = 16;
+    }
+    
+        
+    }
+     
+
--- src/main/java/frc/robot/subsystems/TimeOfFlightSensor.java
+++ src/main/java/frc/robot/subsystems/TimeOfFlightSensor.java
@@ -1,6 +1,12 @@
 package frc.robot.subsystems;
+import frc.robot.Constants.ToFConstants;;
+import com.playingwithfusion.TimeOfFlight;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+public class TimeOfFlightSensor extends SubsystemBase{
+    private TimeOfFlight sensor = new TimeOfFlight(ToFConstants.SensorId);
+    public TimeOfFlightSensor(){
 
-public class TimeOfFlightSensor {
+    }
 
 }
