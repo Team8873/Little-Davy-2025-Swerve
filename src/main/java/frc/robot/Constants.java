c1790e81532642244c41380a808282f5048d02c9
Aryanpunjabi99 <12728@mp.edzone.net>
2025-02-25 16:15:20 -0500

    Update Constants.java
    whats left:
    
    mess with PID
    mess with servo.set
    get servo id
    mess with speed on down arrow

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -74,26 +74,27 @@ public class Constants {
 
     }
     public static class ClimberConstants {
+        //gearbox stuff
         //every deg is about 42/360 which is 0.1167
         //Ticks per deg converts from deg to ticks
         public static final double ticksPerDegClimber = 42/360;
-        //a 100:1 motor
+        //a 100:1 gearbox
         public static final double motorRatioClimberMultiplier = 100;
 
-        //1 DEG of climber climber : (pi*H)/180y) rotations of spool
-        //Y is circumference of spool and H is length of climber (measured to the point where the rope is tied)
-        //right now Y is 3.125inches and H is ___
-        public static final double climberLength = 0;
-        public static final double circumferenceOfSpool = 3.125;
+        //it takes approxamitely a little over 3 spool rotations to go from engaged-> resting
+        //a little less than 2 to go from climbed -> engaged
+        public static final double spoolRotationsRestingToEngaged = 3.2;
+        public static final double spoolRotationsEngagedToClimbed = 1.9;
+        //360 deg is one spool rotations       
    
-        //defined positions; now everythings in ticks
-        public static final double restingPosition = 0*(3.141592654*climberLength)/(180*circumferenceOfSpool)*ticksPerDegClimber*motorRatioClimberMultiplier;
-        public static final double engagedPosition = 90*(3.141592654*climberLength)/(180*circumferenceOfSpool)*ticksPerDegClimber*motorRatioClimberMultiplier;
-        public static final double climbedPositon = 45*(3.141592654*climberLength)/(180*circumferenceOfSpool)*ticksPerDegClimber*motorRatioClimberMultiplier;
-
+        //defined positions; everythings in ticks
+        //360 deg is 1 spool rotation so multiply 360 by that many spool rotations
+        public static final double restingPosition = 0*ticksPerDegClimber*motorRatioClimberMultiplier;
+        public static final double engagedPosition = restingPosition + 360*spoolRotationsRestingToEngaged*ticksPerDegClimber*motorRatioClimberMultiplier;
+        public static final double climbedPositon = restingPosition + 360*spoolRotationsEngagedToClimbed*ticksPerDegClimber*motorRatioClimberMultiplier;
 
         //PID constants:
-        public static final double ClimberkP = 0;
+        public static final double ClimberkP = 0.1;
         public static final double ClimberkI = 0;
         public static final double ClimberkD = 0;
 
