c848a106b1c8983916b1a33b920b8ecbcbb1d288
Coolchameleon8 <marcinek.damian@gmail.com>
2025-03-04 18:05:35 -0500

    joystick squared scaling

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -69,6 +69,11 @@ public class RobotContainer {
     public final LimeLightFace limeLightFace  = new LimeLightFace();
     public final CANdleSystem caNdleSystem = new CANdleSystem(joystick);
 
+    public double DriveBoost = 0.5+(joystick.getRightTriggerAxis()*0.5);
+    public double LeftJoystickXScale = Math.copySign(-joystick.getLeftY()*-joystick.getLeftY(), -joystick.getLeftY());
+    public double LeftJoystickYScale = Math.copySign(-joystick.getLeftX()*-joystick.getLeftX(), -joystick.getLeftX());
+    public double RightJoystickXScale = Math.copySign(-joystick.getRightX()*-joystick.getRightX(), -joystick.getRightX());
+
     /* Path follower */
     private final SendableChooser<Command> autoChooser;
 
@@ -125,9 +130,9 @@ public class RobotContainer {
             // Drivetrain will execute this command periodically
             
             drivetrain.applyRequest(() ->
-                drive.withVelocityX((-joystick.getLeftY()*-joystick.getLeftY() * -joystick.getLeftY()*MaxSpeed)*(0.5+(joystick.getRightTriggerAxis()*0.5))) // Drive forward with negative Y (forward)
-                     .withVelocityY((-joystick.getLeftX()*-joystick.getLeftX()* -joystick.getLeftX()* MaxSpeed)*(0.5+(joystick.getRightTriggerAxis()*0.5))) // Drive left with negative X (left)
-                     .withRotationalRate((-joystick.getRightX()*-joystick.getRightX() *-joystick.getRightX()* MaxAngularRate)*(0.5+(joystick.getRightTriggerAxis()*0.5))) // Drive counterclockwise with negative X (left)
+                drive.withVelocityX((LeftJoystickXScale*MaxSpeed)*(DriveBoost)) // Drive forward with negative Y (forward)
+                     .withVelocityY((LeftJoystickYScale* MaxSpeed)*(DriveBoost)) // Drive left with negative X (left)
+                     .withRotationalRate((RightJoystickXScale* MaxAngularRate)*(DriveBoost)) // Drive counterclockwise with negative X (left)
             )
         );
 
