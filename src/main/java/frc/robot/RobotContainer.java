a6e4ea01a68b8f754de7226c97bb14eb468a4785
Coolchameleon8 <marcinek.damian@gmail.com>
2025-03-04 18:32:09 -0500

    no climber + squared drive

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,8 +6,6 @@ package frc.robot;
 
 import static edu.wpi.first.units.Units.*;
 
-
-
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
@@ -31,7 +29,6 @@ import frc.robot.subsystems.CANdleSystem;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.TimeOfFlightSensor;
-import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.LimeLightFace;
@@ -42,10 +39,12 @@ import frc.robot.command.ArmDockCommand;
 import frc.robot.command.ActiveHumanIntakeCommand;
 import frc.robot.command.DockHumanIntakeCommand;
 
-
 public class RobotContainer {
-    public static final double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
-    public static final double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
+    public static final double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired
+                                                                                              // top speed
+    public static final double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation
+                                                                                                  // per second max
+                                                                                                  // angular velocity
     /* Setting up bindings for necessary control of the swerve drive platform */
     private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
             .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
@@ -65,14 +64,8 @@ public class RobotContainer {
     public final Intake intake = new Intake();
     public final TimeOfFlightSensor tOFSensor = new TimeOfFlightSensor();
     public final Elevator elevator = new Elevator();
-    public final Climber climber = new Climber();
-    public final LimeLightFace limeLightFace  = new LimeLightFace();
+    public final LimeLightFace limeLightFace = new LimeLightFace();
     public final CANdleSystem caNdleSystem = new CANdleSystem(joystick);
-
-    public double DriveBoost = 0.5+(joystick.getRightTriggerAxis()*0.5);
-    public double LeftJoystickXScale = Math.copySign(-joystick.getLeftY()*-joystick.getLeftY(), -joystick.getLeftY());
-    public double LeftJoystickYScale = Math.copySign(-joystick.getLeftX()*-joystick.getLeftX(), -joystick.getLeftX());
-    public double RightJoystickXScale = Math.copySign(-joystick.getRightX()*-joystick.getRightX(), -joystick.getRightX());
 
     /* Path follower */
     private final SendableChooser<Command> autoChooser;
@@ -88,18 +81,20 @@ public class RobotContainer {
         // Note that X is defined as forward according to WPILib convention,
         // and Y is defined as to the left according to WPILib convention.
 
-        
         intake.setDefaultCommand(intake.moveIntake(operator));
 
-        //If sensor detects something close holds
-        //tOFSensor.coralInRange.whileTrue(intake.holdIntake());//commented this out w/ the threshold change
-        
-        //operator.rightTrigger(.2).and(tOFSensor.coralInRange).onTrue(new IntakeEjectCommand(intake, tOFSensor).andThen(new ElevatorPresetCommand(elevator, intake, arm, 'a', false)));
+        // If sensor detects something close holds
+        // tOFSensor.coralInRange.whileTrue(intake.holdIntake());//commented this out w/
+        // the threshold change
+
+        // operator.rightTrigger(.2).and(tOFSensor.coralInRange).onTrue(new
+        // IntakeEjectCommand(intake, tOFSensor).andThen(new
+        // ElevatorPresetCommand(elevator, intake, arm, 'a', false)));
 
         arm.setDefaultCommand(arm.moveArm(operator));
         tOFSensor.setDefaultCommand(tOFSensor.getDistance());
         elevator.setDefaultCommand(elevator.moveElevator(operator));
-        
+
         operator.a().debounce(0.3).whileTrue(new ElevatorPresetCommand(elevator, arm, 'a', false).withTimeout(5));
         operator.b().debounce(0.3).whileTrue(new ElevatorPresetCommand(elevator, arm, 'b', false).withTimeout(5));
         operator.x().debounce(0.3).whileTrue(new ElevatorPresetCommand(elevator, arm, 'x', false).withTimeout(5));
@@ -107,58 +102,65 @@ public class RobotContainer {
         operator.pov(270).debounce(0.3).whileTrue(new ElevatorPresetCommand(elevator, arm, 't', false).withTimeout(5));
         operator.pov(0).debounce(0.3).whileTrue(new ElevatorPresetCommand(elevator, arm, 'h', true).withTimeout(5));
 
-        // operator.leftBumper().negate().and(operator.y().onTrue(new ElevatorPresetCommand(elevator, intake, arm, 'y', false)));
+        // operator.leftBumper().negate().and(operator.y().onTrue(new
+        // ElevatorPresetCommand(elevator, intake, arm, 'y', false)));
 
-        // operator.leftBumper().and(operator.a().onTrue(new ElevatorPresetCommand(elevator, intake, arm, 'a', true)));
-        // operator.leftBumper().and(operator.b().onTrue(new ElevatorPresetCommand(elevator, intake, arm, 'b', true)));
-        // operator.leftBumper().and(operator.x().onTrue(new ElevatorPresetCommand(elevator, intake, arm, 'x', true)));
+        // operator.leftBumper().and(operator.a().onTrue(new
+        // ElevatorPresetCommand(elevator, intake, arm, 'a', true)));
+        // operator.leftBumper().and(operator.b().onTrue(new
+        // ElevatorPresetCommand(elevator, intake, arm, 'b', true)));
+        // operator.leftBumper().and(operator.x().onTrue(new
+        // ElevatorPresetCommand(elevator, intake, arm, 'x', true)));
 
-        //operator.pov(0).onTrue(new StopCommandsCommand(elevator, intake, arm));
+        // operator.pov(0).onTrue(new StopCommandsCommand(elevator, intake, arm));
 
         // operator.back().toggleOnTrue(new DockHumanIntakeCommand(intake));
         // operator.back().toggleOnFalse(new ActiveHumanIntakeCommand(intake));
-         operator.button(9).onTrue(new FlipWristCommand(arm).withTimeout(5));
-         operator.button(10).onTrue(new FlipWrist90Command(arm).withTimeout(3));
-        
-        //operator.pov(180).onTrue(new ElevatorPresetCommand(elevator, intake, arm, 'g', false));
+        operator.button(9).onTrue(new FlipWristCommand(arm).withTimeout(5));
+        operator.button(10).onTrue(new FlipWrist90Command(arm).withTimeout(3));
 
-        //climber stuff:
-        joystick.y().onTrue(climber.moveToEngaged());
-        joystick.x().onTrue(climber.moveClimberDown());
-        joystick.x().onFalse(climber.dontMoveClimberDown());
+        // operator.pov(180).onTrue(new ElevatorPresetCommand(elevator, intake, arm,
+        // 'g', false));
+
+        // climber stuff:
+
         drivetrain.setDefaultCommand(
-            // Drivetrain will execute this command periodically
-            
-            drivetrain.applyRequest(() ->
-                drive.withVelocityX((LeftJoystickXScale*MaxSpeed)*(DriveBoost)) // Drive forward with negative Y (forward)
-                     .withVelocityY((LeftJoystickYScale* MaxSpeed)*(DriveBoost)) // Drive left with negative X (left)
-                     .withRotationalRate((RightJoystickXScale* MaxAngularRate)*(DriveBoost)) // Drive counterclockwise with negative X (left)
-            )
-        );
+                // Drivetrain will execute this command periodically
+
+                drivetrain.applyRequest(() -> {
+                    double DriveBoost = 0.5 + (joystick.getRightTriggerAxis() * 0.5);
+                    double LeftJoystickXScale = Math.copySign(-joystick.getLeftY() * -joystick.getLeftY(),
+                            -joystick.getLeftY());
+                    double LeftJoystickYScale = Math.copySign(-joystick.getLeftX() * -joystick.getLeftX(),
+                            -joystick.getLeftX());
+                    double RightJoystickXScale = Math.copySign(-joystick.getRightX() * -joystick.getRightX(),
+                            -joystick.getRightX());
+                    return drive.withVelocityX((LeftJoystickXScale * MaxSpeed) * (DriveBoost)) // Drive forward with
+                                                                                               // negative Y (forward)
+                            .withVelocityY((LeftJoystickYScale * MaxSpeed) * (DriveBoost)) // Drive left with negative X
+                                                                                           // (left)
+                            .withRotationalRate((RightJoystickXScale * MaxAngularRate) * (DriveBoost)); // Drive
+                                                                                                        // counterclockwise
+                                                                                                        // with negative
+                                                                                                        // X (left)
+                }));
 
         joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
-        joystick.b().whileTrue(drivetrain.applyRequest(() ->
-            point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
-        ));
-        joystick.pov(0).whileTrue(drivetrain.applyRequest(() ->
-            forwardStraight.withVelocityX(0.5).withVelocityY(0))
-        );
-        joystick.pov(180).whileTrue(drivetrain.applyRequest(() ->
-            forwardStraight.withVelocityX(-0.5).withVelocityY(0))
-        );
-        joystick.rightStick().whileTrue(lockOnCommand()
-        );
-        joystick.pov(90).whileTrue(drivetrain.applyRequest(()->
-            forwardStraight.withVelocityX(0).withVelocityY(limeLightFace.limelight_right_strafe_proportional() ))
-        );
-        joystick.pov(270).whileTrue(drivetrain.applyRequest(()->
-            forwardStraight.withVelocityX(0).withVelocityY(limeLightFace.limelight_left_strafe_proportional() )));
-            
+        joystick.b().whileTrue(drivetrain.applyRequest(
+                () -> point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))));
+        joystick.pov(0).whileTrue(drivetrain.applyRequest(() -> forwardStraight.withVelocityX(0.5).withVelocityY(0)));
+        joystick.pov(180)
+                .whileTrue(drivetrain.applyRequest(() -> forwardStraight.withVelocityX(-0.5).withVelocityY(0)));
+        joystick.rightStick().whileTrue(lockOnCommand());
+        joystick.pov(90).whileTrue(drivetrain.applyRequest(() -> forwardStraight.withVelocityX(0)
+                .withVelocityY(limeLightFace.limelight_right_strafe_proportional())));
+        joystick.pov(270).whileTrue(drivetrain.applyRequest(() -> forwardStraight.withVelocityX(0)
+                .withVelocityY(limeLightFace.limelight_left_strafe_proportional())));
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
-       joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-       joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+        joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+        joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
         joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
         joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
 
@@ -166,30 +168,36 @@ public class RobotContainer {
         joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
 
         drivetrain.registerTelemetry(logger::telemeterize);
-        
+
     }
-    public SequentialCommandGroup lockOnCommand(){
-           return new SequentialCommandGroup(drivetrain.applyRequest(() ->
-           drive.withRotationalRate(limeLightFace.limelight_aim_proportional())).withTimeout(.5)
-           .andThen(
-               drivetrain.applyRequest(()-> 
-               forwardStraight.withVelocityX(limeLightFace.limelight_range_proportional() * 0.005))));
+
+    public SequentialCommandGroup lockOnCommand() {
+        return new SequentialCommandGroup(
+                drivetrain.applyRequest(() -> drive.withRotationalRate(limeLightFace.limelight_aim_proportional()))
+                        .withTimeout(.5)
+                        .andThen(
+                                drivetrain.applyRequest(() -> forwardStraight
+                                        .withVelocityX(limeLightFace.limelight_range_proportional() * 0.005))));
     }
-    public SequentialCommandGroup strafeRightCommand(){
-        return new SequentialCommandGroup(drivetrain.applyRequest(() ->
-        forwardStraight.withVelocityX(0).withVelocityY(limeLightFace.limelight_right_strafe_proportional() * 0.01)).withTimeout(.5)
-    );
- }
- public SequentialCommandGroup strafeLeftCommand(){
-    return new SequentialCommandGroup(drivetrain.applyRequest(() ->
-    forwardStraight.withVelocityX(0).withVelocityY(limeLightFace.limelight_left_strafe_proportional() * 0.01)).withTimeout(.5)
-    );
- }
+
+    public SequentialCommandGroup strafeRightCommand() {
+        return new SequentialCommandGroup(
+                drivetrain
+                        .applyRequest(() -> forwardStraight.withVelocityX(0)
+                                .withVelocityY(limeLightFace.limelight_right_strafe_proportional() * 0.01))
+                        .withTimeout(.5));
+    }
+
+    public SequentialCommandGroup strafeLeftCommand() {
+        return new SequentialCommandGroup(
+                drivetrain
+                        .applyRequest(() -> forwardStraight.withVelocityX(0)
+                                .withVelocityY(limeLightFace.limelight_left_strafe_proportional() * 0.01))
+                        .withTimeout(.5));
+    }
 
     public Command getAutonomousCommand() {
         /* Run the path selected from the auto chooser */
         return autoChooser.getSelected();
     }
 }
-
-
