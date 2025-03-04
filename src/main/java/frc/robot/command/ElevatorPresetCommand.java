e126a557cf56c522004f97687cbd2be3a856b052
8873DriverStation <8873DriverStation@gmail.com>
2025-03-04 18:16:38 -0500

    Format

--- src/main/java/frc/robot/command/ElevatorPresetCommand.java
+++ src/main/java/frc/robot/command/ElevatorPresetCommand.java
@@ -10,106 +10,106 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants.PresetConstants;
 
-/** An  command that uses an  subsystem. */
 public class ElevatorPresetCommand extends Command {
-  private final Elevator m_elevator;
-  private final Arm m_arm;
-  private double wristPos = PresetConstants.wristFlatPos;
-  private double armPos = PresetConstants.lvl1to3ArmPos;
-  private double elevatorPos = PresetConstants.lvl1Elevator;
-  private char m_button_pressed = 'a';
-  private boolean m_wristSide = false;
-  private int timer = 0;
-  /**
-   * Creates a new Command.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
-  public ElevatorPresetCommand(Elevator elevator, Arm arm, char button, boolean wristSide) {
-    m_elevator = elevator;  //saves a local reference to the elevator subsystem
-    m_arm = arm;  //saves a local reference to the arm subsystem
-    m_button_pressed = button;  //saves a local reference to what button was pressed 
-    m_wristSide  = wristSide; //saves a local reference if whether the wirst needs to be on its side or not
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(elevator,arm);
-    getInterruptionBehavior();
-  }
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-    m_elevator.resetPidError();
-    m_arm.resetPidError();
-    checkPresetLvl();
-    m_elevator.targetElevatorPosition(elevatorPos);
-    m_arm.setArmMechTarget(wristPos, armPos);
-  }
+    private final Elevator m_elevator;
+    private final Arm m_arm;
+    private double wristPos = PresetConstants.wristFlatPos;
+    private double armPos = PresetConstants.lvl1to3ArmPos;
+    private double elevatorPos = PresetConstants.lvl1Elevator;
+    private char m_button_pressed = 'a';
+    private boolean m_wristSide = false;
+    private int timer = 0;
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    m_elevator.elevatorPreset();
-
-    if(timer < 25) {
-      timer++; 
-      return;
+    public ElevatorPresetCommand(Elevator elevator, Arm arm, char button, boolean wristSide) {
+        m_elevator = elevator; // saves a local reference to the elevator subsystem
+        m_arm = arm; // saves a local reference to the arm subsystem
+        m_button_pressed = button; // saves a local reference to what button was pressed
+        m_wristSide = wristSide; // saves a local reference if whether the wirst needs to be on its side or not
+        // Use addRequirements() here to declare subsystem dependencies.
+        addRequirements(elevator, arm);
     }
 
-    m_arm.setArmSpeed();
- }
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        m_elevator.resetPidError();
+        m_arm.resetPidError();
+        checkPresetLvl();
+        m_elevator.targetElevatorPosition(elevatorPos);
+        m_arm.setArmMechTarget(wristPos, armPos);
+    }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-    timer = 0;
-  }
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        m_elevator.updateElevatorPID();
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return m_elevator.elevatorAtTarget.getAsBoolean() && m_arm.armMechAtTarget.getAsBoolean();
-  }
-
-  /**
-   * takes the wristSide boolean and button pressed to determine which preset level to use
-   */
-  private void checkPresetLvl(){
-    if(m_wristSide){
-      armPos = PresetConstants.lvl1to3ArmPosSide;
-      wristPos = PresetConstants.wristSidePos;
-      switch (m_button_pressed) {
-        // case 'a': elevatorPos = PresetConstants.lvl1ElevatorSide;
-        //   break;
-        case 'b': elevatorPos = PresetConstants.lvl2ElevatorSide;
-          break;
-        case 'x': elevatorPos = PresetConstants.lvl3ElevatorSide;
-          break;
-        case'h': elevatorPos = PresetConstants.humanElevatorPos;
-                 armPos = PresetConstants.humanArmPos;
-                 break;
-      }}
-      
-    else{
-        switch (m_button_pressed) {
-
-        case 'b': elevatorPos = PresetConstants.lvl2Elevator;
-          break;
-
-        case 'x': elevatorPos = PresetConstants.lvl3Elevator;
-          break;
-
-        case 'y': elevatorPos = PresetConstants.lvl4Elevator;
-                armPos = PresetConstants.lvl4ArmPos;
-                break;
-
-        case'g': elevatorPos = PresetConstants.goundElevator;
-                armPos = PresetConstants.groundArm;
-                wristPos = PresetConstants.wristSidePos;
-          break;
-        case't': elevatorPos = PresetConstants.goundElevator;
-                  armPos = PresetConstants.travelArm;
+        if (timer < 25) {
+            timer++;
+            return;
         }
-      }
 
-  }
+        m_arm.setArmSpeed();
+    }
 
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        timer = 0;
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return m_elevator.elevatorAtTarget.getAsBoolean() && m_arm.armMechAtTarget.getAsBoolean();
+    }
+
+    /**
+     * takes the wristSide boolean and button pressed to determine which preset
+     * level to use
+     */
+    private void checkPresetLvl() {
+        if (m_wristSide) {
+            wristPos = PresetConstants.wristSidePos;
+            switch (m_button_pressed) {
+                // case 'a': elevatorPos = PresetConstants.lvl1ElevatorSide;
+                // break;
+                case 'b':
+                    elevatorPos = PresetConstants.lvl2ElevatorSide;
+                    break;
+                case 'x':
+                    elevatorPos = PresetConstants.lvl3ElevatorSide;
+                    break;
+                case 'h':
+                    elevatorPos = PresetConstants.humanElevatorPos;
+                    armPos = PresetConstants.humanArmPos;
+                    break;
+            }
+        } else {
+            switch (m_button_pressed) {
+
+                case 'b':
+                    elevatorPos = PresetConstants.lvl2Elevator;
+                    break;
+
+                case 'x':
+                    elevatorPos = PresetConstants.lvl3Elevator;
+                    break;
+
+                case 'y':
+                    elevatorPos = PresetConstants.lvl4Elevator;
+                    armPos = PresetConstants.lvl4ArmPos;
+                    break;
+
+                case 'g':
+                    elevatorPos = PresetConstants.goundElevator;
+                    armPos = PresetConstants.groundArm;
+                    wristPos = PresetConstants.wristSidePos;
+                    break;
+                case 't':
+                    elevatorPos = PresetConstants.goundElevator;
+                    armPos = PresetConstants.travelArm;
+            }
+        }
+    }
 }
--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,4 +1,5 @@
 package frc.robot.subsystems;
+
 import java.util.function.BooleanSupplier;
 
 import com.revrobotics.RelativeEncoder;
@@ -20,21 +21,21 @@ import frc.robot.Constants.ElevatorConstants;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
 
+public class Elevator extends SubsystemBase {
 
-public class Elevator extends SubsystemBase{
-
-    // creates Sparkmax object 
+    // creates Sparkmax object
     private final SparkMax motorLeft = new SparkMax(ElevatorConstants.elevatorLCanId, MotorType.kBrushless);
     private final SparkMax leadMotorRight = new SparkMax(ElevatorConstants.elevatorRCanId, MotorType.kBrushless);
     // creates private variables
-    private BooleanSupplier elevatorAtSetpoint = ()-> false;
-    private double speed = 0; 
-    private double elevatorRightPos= 0;
+    private BooleanSupplier elevatorAtSetpoint = () -> false;
+    private double speed = 0;
+    private double elevatorRightPos = 0;
     private double elevatorLeftPos = 0;
     private double elevatorRightVel = 0;
     private double elevatorLeftVel = 0;
@@ -43,130 +44,127 @@ public class Elevator extends SubsystemBase{
     // shuffleboard thing I don't know what shuffleboard is
     private ShuffleboardTab tab = Shuffleboard.getTab("Subsystems");
 
-    private GenericEntry speedEntry =
-      tab.add("Elevator Speed", 0)
-         .withWidget(BuiltInWidgets.kNumberBar)
-         .withPosition(0,1)
-         .getEntry();
+    private GenericEntry speedEntry = tab.add("Elevator Speed", 0)
+            .withWidget(BuiltInWidgets.kNumberBar)
+            .withPosition(0, 1)
+            .getEntry();
 
-    private GenericEntry positionEntry =
-      tab.add("Elevator position", 0)
-         .withWidget(BuiltInWidgets.kNumberBar)
-         .withPosition(2,0)
-         .getEntry();
+    private GenericEntry positionEntry = tab.add("Elevator position", 0)
+            .withWidget(BuiltInWidgets.kNumberBar)
+            .withPosition(2, 0)
+            .getEntry();
 
-    private GenericEntry targetEntry =
-      tab.add("Elevator busVolt", 0)
-         .withWidget(BuiltInWidgets.kNumberBar)
-         .withPosition(2,1)
-         .getEntry();
+    private GenericEntry targetEntry = tab.add("Elevator busVolt", 0)
+            .withWidget(BuiltInWidgets.kNumberBar)
+            .withPosition(2, 1)
+            .getEntry();
 
-    private GenericEntry elevatorSetpointWidget =
-      tab.add("Elevator atSetpoint", false)
-         .withWidget(BuiltInWidgets.kBooleanBox)
-         .withPosition(3,5)
-         .getEntry();
-         
-    private GenericEntry elevatorPast =
-      tab.add("Elevator Past", 0)
-         .withWidget(BuiltInWidgets.kNumberBar)
-         .withPosition(4,5)
-         .getEntry();
-    
+    private GenericEntry elevatorSetpointWidget = tab.add("Elevator atSetpoint", false)
+            .withWidget(BuiltInWidgets.kBooleanBox)
+            .withPosition(3, 5)
+            .getEntry();
 
-        
-    //gets the encoders and creates a object to talk to the encoders
-    //private RelativeEncoder elevatorEncoder = motorLeft.getEncoder();
+    private GenericEntry elevatorPast = tab.add("Elevator Past", 0)
+            .withWidget(BuiltInWidgets.kNumberBar)
+            .withPosition(4, 5)
+            .getEntry();
+
+    // gets the encoders and creates a object to talk to the encoders
+    // private RelativeEncoder elevatorEncoder = motorLeft.getEncoder();
     private RelativeEncoder elevatorEncoder = leadMotorRight.getEncoder();
     private RelativeEncoder elevatorLeftEncoder = motorLeft.getEncoder();
 
-    //creates PID loop
-    private final ProfiledPIDController elevatorPid = new ProfiledPIDController(ElevatorConstants.kP, ElevatorConstants.kI, ElevatorConstants.kD, 
-    new TrapezoidProfile.Constraints(ElevatorConstants.maxVelocity,ElevatorConstants.maxAcceleration));
-    //private final ElevatorFeedforward m_feedforward = new ElevatorFeedforward(ElevatorConstants.kS, ElevatorConstants.kG, ElevatorConstants.kV);
-    private ComplexWidget pidEntry =
-      tab.add("Elevator Pid", elevatorPid)
-        .withWidget(BuiltInWidgets.kPIDController)
-         .withPosition(6,1);
-         
-    //defines variables to 0 
+    // creates PID loop
+    private final ProfiledPIDController elevatorPid = new ProfiledPIDController(ElevatorConstants.kP,
+            ElevatorConstants.kI, ElevatorConstants.kD,
+            new TrapezoidProfile.Constraints(ElevatorConstants.maxVelocity, ElevatorConstants.maxAcceleration));
+    // private final ElevatorFeedforward m_feedforward = new
+    // ElevatorFeedforward(ElevatorConstants.kS, ElevatorConstants.kG,
+    // ElevatorConstants.kV);
+    private ComplexWidget pidEntry = tab.add("Elevator Pid", elevatorPid)
+            .withWidget(BuiltInWidgets.kPIDController)
+            .withPosition(6, 1);
+
+    // defines variables to 0
     private double elevatorPosition = 0;
     private State targetPosition;
     private double pastPosition = 0;
     private double elevatorTarget = 0;
-    public Elevator(){
-      elevatorPid.disableContinuousInput();
+
+    public Elevator() {
+        elevatorPid.disableContinuousInput();
     }
 
-    //sets follower
-    public void setFollower(){
+    // sets follower
+    public void setFollower() {
         SparkMaxConfig globalConfig = new SparkMaxConfig();
         SparkMaxConfig FollowerConfig = new SparkMaxConfig();
-    
+
         /*
          * Set parameters that will apply to all SPARKs. We will also use this as
          * the left leader config.
          */
         globalConfig
-            .smartCurrentLimit(40)
-            .idleMode(IdleMode.kBrake);
-    
+                .smartCurrentLimit(40)
+                .idleMode(IdleMode.kBrake);
+
         // Apply the global config and invert since it is on the opposite side
         FollowerConfig
-            .apply(globalConfig)
-            .inverted(true)
-            ;
+                .apply(globalConfig)
+                .inverted(true);
 
-             /*
-     * Apply the configuration to the SPARKs.
-     *
-     * kResetSafeParameters is used to get the SPARK MAX to a known state. This
-     * is useful in case the SPARK MAX is replaced.
-     *
-     * kPersistParameters is used to ensure the configuration is not lost when
-     * the SPARK MAX loses power. This is useful for power cycles that may occur
-     * mid-operation.
-     */
+        /*
+         * Apply the configuration to the SPARKs.
+         *
+         * kResetSafeParameters is used to get the SPARK MAX to a known state. This
+         * is useful in case the SPARK MAX is replaced.
+         *
+         * kPersistParameters is used to ensure the configuration is not lost when
+         * the SPARK MAX loses power. This is useful for power cycles that may occur
+         * mid-operation.
+         */
         leadMotorRight.configure(globalConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
         motorLeft.configure(FollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
     }
-    
+
     /**
      * @param operator the joystick to read from
      * @return the action/method to run
      */
-    public Command moveElevator(CommandXboxController operator){
+    public Command moveElevator(CommandXboxController operator) {
         return this.run(
-            () -> {
-            readFromController(operator);
-            });
+                () -> {
+                    readFromController(operator);
+                });
     }
 
     /**
-     * Calls targetposition and sets the target to be the y values of the left joystick 
+     * Calls targetposition and sets the target to be the y values of the left
+     * joystick
      * sets elevator motor speed
+     * 
      * @param operator the joystick to read from
      */
-    private void readFromController(CommandXboxController operator){
-        elevatorTarget += (operator.getRightY()/160);
+    private void readFromController(CommandXboxController operator) {
+        elevatorTarget += (operator.getRightY() / 160);
         targetElevatorPosition(elevatorTarget);
-        elevatorPreset();
+        updateElevatorPID();
     }
 
     /**
-     * @return while the elevator pid is NOT at the setpoint it runs the motor
+     * Calculate speed using elevator PID and sets elevator speed
      */
-    public void elevatorPreset (){
+    public void updateElevatorPID() {
         speed = elevatorPid.calculate(elevatorPosition);
-        //+ m_feedforward.calculate(elevatorPid.getSetpoint().velocity);
+        // + m_feedforward.calculate(elevatorPid.getSetpoint().velocity);
         leadMotorRight.set(speed);
         motorLeft.set(speed);
     }
-    
+
     /**
      * sets elevator position equal to encoder position
      */
-    private void getEncoderData(){
+    private void getEncoderData() {
         elevatorRightPos = elevatorEncoder.getPosition();
         elevatorLeftPos = elevatorLeftEncoder.getPosition();
         elevatorPosition = (elevatorLeftPos + elevatorRightPos) / 2;
@@ -174,59 +172,60 @@ public class Elevator extends SubsystemBase{
         elevatorRightVel = elevatorEncoder.getVelocity();
         elevatorLeftVel = elevatorLeftEncoder.getVelocity();
 
-        elevatorVelocity = (elevatorLeftVel + elevatorRightVel)/2;
+        elevatorVelocity = (elevatorLeftVel + elevatorRightVel) / 2;
+        elevatorVelocity *= ElevatorConstants.gearRatio;
 
         elevatorPosition *= ElevatorConstants.gearRatio;
-        
-    }
-    
-    /**
-     * sets the setpoint for the Elevator PID controller
-     * @param target the target position
-     */
-    public void targetElevatorPosition(double target){
-        double m_target = target;
-        if(m_target < 0.2){m_target = 0.2;}
-        if(m_target > 4.22){m_target = 4.22;}
-        elevatorPid.setGoal(m_target);
-        elevatorTarget = m_target;
-    }
-    public double getElevatorPos(){
-      return elevatorPosition;
-    }
-    public void resetPidError(){
-        elevatorPid.reset(elevatorPosition, elevatorVelocity);
-    }
-    /**
-     * setting the targetPosition to the current set point 
-     * @return the boolean value if whether the elevator is at the set point or not
-     */
-    public BooleanSupplier getElevatorSetpointStatus(){
-        targetPosition = elevatorPid.getGoal();
-       return elevatorAtSetpoint = ()-> elevatorPid.atSetpoint();
     }
 
-    //creates an unchangeable trigger boolean if whether the elevator is at the setpoint
+    /**
+     * sets the setpoint for the Elevator PID controller
+     * 
+     * @param target the target position
+     */
+    public void targetElevatorPosition(double target) {
+        MathUtil.clamp(target, 0.2, 4.22);
+        elevatorPid.setGoal(new State(target, 0.0));
+        elevatorTarget = target;
+    }
+
+    public double getElevatorPos() {
+        return elevatorPosition;
+    }
+
+    public void resetPidError() {
+        elevatorPid.reset(elevatorPosition, elevatorVelocity);
+    }
+
+    /**
+     * setting the targetPosition to the current set point
+     * 
+     * @return the boolean value if whether the elevator is at the set point or not
+     */
+    public BooleanSupplier getElevatorSetpointStatus() {
+        targetPosition = elevatorPid.getGoal();
+        return elevatorAtSetpoint = () -> elevatorPid.atSetpoint();
+    }
+
+    // creates an unchangeable trigger boolean if whether the elevator is at the
+    // setpoint
     public final Trigger elevatorAtTarget = new Trigger(getElevatorSetpointStatus());
 
     @Override
-  public void periodic(){
-    getEncoderData();
-    getElevatorSetpointStatus();
-    updateShuffleboardWidgets();
-  } 
+    public void periodic() {
+        getEncoderData();
+        getElevatorSetpointStatus();
+        updateShuffleboardWidgets();
+    }
 
-  /**
-   * updates the shuffleboard stuff
-   */
-  private void updateShuffleboardWidgets(){
-    positionEntry.setDouble(elevatorPosition);
-    speedEntry.setDouble(speed);
-    targetEntry.setDouble(leadMotorRight.getBusVoltage());
-    elevatorSetpointWidget.setBoolean(elevatorAtSetpoint.getAsBoolean());
-    elevatorPast.setDouble(pastPosition);
-
-
-  }
-
+    /**
+     * updates the shuffleboard stuff
+     */
+    private void updateShuffleboardWidgets() {
+        positionEntry.setDouble(elevatorPosition);
+        speedEntry.setDouble(speed);
+        targetEntry.setDouble(leadMotorRight.getBusVoltage());
+        elevatorSetpointWidget.setBoolean(elevatorAtSetpoint.getAsBoolean());
+        elevatorPast.setDouble(pastPosition);
+    }
 }
