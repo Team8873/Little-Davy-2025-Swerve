b1cff9382be3cb08471ccc3f8c2ac5bbe259382d
8873DriverStation <8873DriverStation@gmail.com>
2025-03-04 18:58:38 -0500

    human intake arm pos

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -86,7 +86,8 @@ public class Constants {
 
         public static final double travelArm = 0.451;
         public static final double humanElevatorPos = 0.45;
-        public static final double humanArmPos = 0.5;
+        public static final double humanIntakeArmPos = 0.615;
+        public static final double directHumanArmPos = 0.5;
         public static final double wristTolerance = 2;
 
     }
